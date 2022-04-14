f7a6d549bfb4ad11f7c299276fa9a3b2ec4c8a3f
Elliott Arnold <si3mshady@Elliotts-MBP.attlocal.net>
2022-04-14 04:58:34 -0500

    save point

--- main.tf
+++ main.tf
@@ -15,7 +15,8 @@ resource "docker_image" "blockchain_image" {
 }
 
 resource "docker_container" "blockchain_container" {
-  name = join("-",["blockchain_container", random_string.var.result])
+  count = 3
+  name = join("-",["blockchain_container", random_string.var[count.index].result])
   image = docker_image.blockchain_image.latest
   ports {
       internal = 80
@@ -25,17 +26,24 @@ resource "docker_container" "blockchain_container" {
 }
 
 resource "random_string" "var" {
+    count = 3
     length = 5
     special = false
   
 }
 
-output "joined_container_ip_and_port"{
-    value = join(":", [docker_container.blockchain_container.ip_address, docker_container.blockchain_container.ports[0].external])
-  description = "joined container ip and port"
+# output "joined_container_ip_and_port_flatten"{
+#     value = join(":", flatten([docker_container.blockchain_container[*].ip_address, docker_container.blockchain_container[0].ports[0].external]))
+#   description = "joined container ip and port using flatten"
+# }
+
+output "joined_container_ip_and_port_forLoop"{
+    value = [for i in  docker_container.blockchain_container[*]: join(":",[i.ip_address],[i.ports[0].external])]
+  description = "joined container ip and port using flatten"
 }
 
 output "container_name"{
-    value = docker_container.blockchain_container.name
+    
+    value = docker_container.blockchain_container[*].name
   description = "container_name"
 }
